1a2e15947d299847d98c69f683462aadbb341bfd
Laurent Ouma <lawiomosh3@gmail.com>
2024-10-30 11:06:21 +0300

    Setup monorepo

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/next.js"],
+  extends: ["@hive/eslint-config/next.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
