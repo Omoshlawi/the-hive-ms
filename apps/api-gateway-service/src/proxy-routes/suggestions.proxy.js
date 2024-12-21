4b352dd7f11429b936fc7923384df62e8598689d
Laurent Ouma <lawiomosh3@gmail.com>
2024-12-21 22:36:21 +0300

    Add proxy routes for suggestion service with icon families, icons, places, and addresses

---
+++ apps/api-gateway-service/src/proxy-routes/suggestions.proxy.js
@@ -0,0 +1,30 @@
+const serviceName = "@hive/suggestion-service";
+let serviceVersion
+
+module.exports = [
+    {
+      path: "/icon-families",
+      prefix: "/icon-families",
+      serviceName,
+      serviceVersion,
+    },
+    {
+      path: "/icons",
+      prefix: "/icons",
+      serviceName,
+      serviceVersion,
+    },
+    {
+      path: "/places",
+      prefix: "/places",
+      serviceName,
+      serviceVersion,
+    },
+    {
+      path: "/addresses",
+      prefix: "/addresses",
+      serviceName,
+      serviceVersion,
+    },
+  ]
+  
